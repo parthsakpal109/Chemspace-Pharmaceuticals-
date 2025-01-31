f7c99d2c3227ce236755170f5b5db26ba56cfc8a
Jaydeep <jarejaydeep@gmail.com>
2025-01-31 14:17:36 +0530

    unknown

--- db_connection.php
+++ db_connection.php
@@ -1,7 +1,7 @@
 <?php
 require 'vendor/autoload.php'; // Include Composer's autoloader
 
-$uri = "mongodb+srv://jarejaydeep:Bcj3H3XKI0pGoImc@cluster0.fxrir.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+$uri = ;"mongodb+srv://jarejaydeep:Bcj3H3XKI0pGoImc@cluster0.fxrir.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 try {
     $client = new MongoDB\Client($uri);
